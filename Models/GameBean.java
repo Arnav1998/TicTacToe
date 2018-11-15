2edbf99477fdfbe6ed93001c58b7d29ba85a5ae7
Arnav Singhania <arnavsinghania98@gmail.com>
2018-11-14 23:21:27 -0800

    Added a class that defines the game and controls it.

---
+++ Models/GameBean.java
@@ -0,0 +1,85 @@
+package models;
+
+public class GameBean {
+	
+//Your game bean will be responsible for managing all aspects of your tic-tac-toe game. 
+//It will record all moves by all players, keep track of the current player, determine if a move is valid, 
+//and report on whether the game is over, who the winner is, or if there is a draw.
+	
+	private char[][] gameArray;
+	private int moveCount;
+	
+	public GameBean() {
+		
+		this.gameArray = new char[3][3];
+		
+		for (int r = 0; r < this.gameArray.length; r++) {
+			for (int c = 0; c < this.gameArray[r].length; c++) {
+				this.gameArray[r][c] = 'E';
+			}
+		}
+		
+		this.moveCount = 0;
+		
+	}
+	
+	public boolean move(int moveRow, int moveCol) {
+		
+		if (validMove(moveRow, moveCol)) {
+			
+			this.moveCount++;
+			
+			if (moveCount%2 == 0) {
+				this.gameArray[moveRow][moveCol] = 'O';
+			} else {
+				this.gameArray[moveRow][moveCol] = 'X';
+			}
+			
+		} 
+		
+		for (int r = 0; r < this.gameArray.length; r++) {
+			for (int c = 0; c < this.gameArray[r].length; c++) {
+				System.out.print(gameArray[r][c]+" ");
+			}
+			System.out.println();
+		}
+	
+		return gameWinningStatus();
+			
+		
+	}
+	
+	private boolean validMove(int moveRow, int moveCol) {
+		
+		if (this.gameArray[moveRow][moveCol]=='E') 
+			return true;
+		else
+			return false;
+		
+	}
+	
+	private boolean gameWinningStatus() {
+		
+		if (gameArray[0][0] == gameArray[0][1] && gameArray[0][0] == gameArray[0][2] && gameArray[0][0] != 'E') {
+			return true;
+		} else if (gameArray[1][0] == gameArray[1][1] && gameArray[1][0] == gameArray[1][2] && gameArray[1][0] != 'E') {
+			return true;
+		} else if (gameArray[2][0] == gameArray[2][1] && gameArray[2][0] == gameArray[2][2] && gameArray[2][0] != 'E') {
+			return true;
+		} else if (gameArray[0][0] == gameArray[1][0] && gameArray[0][0] == gameArray[2][0] && gameArray[0][0] != 'E') {
+			return true;
+		} else if (gameArray[0][1] == gameArray[1][1] && gameArray[0][1] == gameArray[2][1] && gameArray[0][1] != 'E') {
+			return true;
+		} else if (gameArray[0][2] == gameArray[1][2] && gameArray[0][2] == gameArray[2][2] && gameArray[0][2] != 'E') {
+			return true;
+		} else if (gameArray[0][0] == gameArray[1][1] && gameArray[0][0] == gameArray[2][2] && gameArray[0][0] != 'E') {
+			return true;
+		} else if (gameArray[0][2] == gameArray[1][1] && gameArray[0][2] == gameArray[2][0] && gameArray[0][2] != 'E') {
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+	
+}
