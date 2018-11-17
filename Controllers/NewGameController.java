fb4f84a6e0ab368357cbc045ae3cc637f640e484
Arnav Singhania <arnavsinghania98@gmail.com>
2018-11-16 19:08:24 -0800

    Added functionality to start a new game.

--- Controllers/NewGameController.java
+++ Controllers/NewGameController.java
@@ -11,9 +11,12 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/mvc/NewGameController")
 public class NewGameController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+//Your new game controller is responsible for creating a new game.
    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		request.getSession().removeAttribute("game");
+		request.getRequestDispatcher("GameController").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
