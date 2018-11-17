30f9a7870743dbf219cdef0dfe62d49160adac58
Arnav Singhania <arnavsinghania98@gmail.com>
2018-11-16 18:40:31 -0800

    Added code to PlayController to sync player moves and the GameBean.

--- Controllers/PlayController.java
+++ Controllers/PlayController.java
@@ -7,12 +7,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.GameBean;
+
 @WebServlet("/mvc/PlayController")
 public class PlayController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//Your play controller is responsible for updating your game bean with new moves from the player. 
+//Once it's work is done, there is nothing for it to display, so it makes sense to have it 
+//redirect to your game page (Game Controller).
 	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		GameBean game = (GameBean) request.getSession().getAttribute("game");
+		game.move(Integer.parseInt(request.getParameter("row")), Integer.parseInt(request.getParameter("col")));
+		request.getRequestDispatcher("GameController").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
